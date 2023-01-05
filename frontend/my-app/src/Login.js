0f1044fab4dc7228bdbd113711f3bc5ed9271048
Andreea Bak <bakandreea01@gmail.com>
2023-01-06 00:22:00 +0200

    login updated

--- frontend/my-app/src/Login.js
+++ frontend/my-app/src/Login.js
@@ -2,14 +2,14 @@ import "./styles/login.css"
 
 const Login = () => {
     return ( 
-        <div >
+        <div className="container">
             <div className="containerLog">
                     <div className="borderWhite"> 
                     <p >LOGIN</p></div>
                     
-                    <p1>email</p1>
+                    <p>email</p>
                     <input className="inputLogin" type="text" id="login_input" placeholder="email"></input>
-                    <p1>password</p1>
+                    <p>password</p>
                     <input className="inputLogin" type="text" id="login_input" placeholder="password"></input>
                     <button>Login</button>
             
